0097cabbe769954c0097a0d0d4669054f757ef85
Rolf Streefkerk <r.streefkerk@gmail.com>
2020-04-28 15:12:10 +0700

    Lambda alarms added

---
+++ main.tf
@@ -0,0 +1,28 @@
+# -----------------------------------------------------------------------------
+# Module: CloudWatch Alarms Lambda
+# -----------------------------------------------------------------------------
+module "lambda" {
+  source = "./modules/lambda"
+
+  namespace         = var.namespace
+  region            = var.region
+  resource_tag_name = var.resource_tag_name
+
+  create_errorRate_alarm      = var.create_errorRate_alarm
+  errorRate_threshold         = var.errorRate_threshold
+  errorRate_evaluationPeriods = var.errorRate_evaluationPeriods
+
+  create_throttleCount_alarm      = var.create_throttleCount_alarm
+  throttleCount_threshold         = var.throttleCount_threshold
+  throttleCount_evaluationPeriods = var.throttleCount_evaluationPeriods
+
+  create_iteratorAge_alarm      = var.create_iteratorAge_alarm
+  iteratorAge_threshold         = var.iteratorAge_threshold
+  iteratorAge_evaluationPeriods = var.iteratorAge_evaluationPeriods
+
+  create_deadLetterQueue_alarm      = var.create_deadLetterQueue_alarm
+  deadLetterQueue_threshold         = var.deadLetterQueue_threshold
+  deadLetterQueue_evaluationPeriods = var.deadLetterQueue_evaluationPeriods
+
+  function_name = var.lambda_function_name
+}
